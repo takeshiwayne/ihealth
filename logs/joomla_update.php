e66a235fc0906f8d8961abad384445c51a840859
takeshiwayne <wayne@bm-sms.com.tw>
2014-01-06 09:52:31 +0800

    first commit

---
+++ logs/joomla_update.php
@@ -0,0 +1,15 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-01-06 01:41:46 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-01-06T01:41:46+00:00	INFO	update	Update started by user Super User (728). Old version is 3.2.0.
+2014-01-06T01:41:46+00:00	INFO	update	Downloading update file from .
+2014-01-06T01:41:50+00:00	INFO	update	File Joomla_3.2.x_to_3.2.1-Stable-Patch_Package.zip successfully downloaded.
+2014-01-06T01:41:51+00:00	INFO	update	Starting installation of new version.
+2014-01-06T01:41:55+00:00	INFO	update	Finalising installation.
+2014-01-06T01:41:55+00:00	INFO	update	Ran query from file 3.2.1. Query text: DELETE FROM `#__postinstall_messages` WHERE `title_key` = 'PLG_USER_JOOMLA_POSTI.
+2014-01-06T01:41:55+00:00	INFO	update	Deleting removed files and folders.
+2014-01-06T01:41:56+00:00	INFO	update	Cleaning up after installation.
+2014-01-06T01:41:56+00:00	INFO	update	Update to version 3.2.1 is complete.
